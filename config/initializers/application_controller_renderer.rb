335314b6c8fa36b4bb93b74f6e94b72d55bf84d0
i-tanaka730 <pttp730@gmail.com>
2021-01-27 23:21:25 +0900

    Refileを使用できるように対応。

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'e42e6e5144f9ba52b2d8bc53f1a03e67a43d391e200c734869b9a65cdbe8642e40efe810d6f07e0de7e36f1cba9d1bac1cb8fd2267b4dfc57114c89cede2d8ed'
